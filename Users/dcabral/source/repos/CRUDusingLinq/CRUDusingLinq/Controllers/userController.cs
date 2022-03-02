a75fa34aa647f87f840cae8174cef19341af093b
Darwin <dcabral@suyati.com>
2022-03-02 11:21:57 +0530

    views added

--- Users/dcabral/source/repos/CRUDusingLinq/CRUDusingLinq/Controllers/userController.cs
+++ Users/dcabral/source/repos/CRUDusingLinq/CRUDusingLinq/Controllers/userController.cs
@@ -50,5 +50,65 @@ namespace CRUDusingLinq.Controllers
             return View(user);
 
         }
+        public ActionResult Index()
+        {
+            var users = _repository.GetUserDatas();
+            return View(users);
+        }
+        public ActionResult Details(int id)
+        {
+            userdata model = _repository.GetuserByID(id);
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            userdata model = _repository.GetuserByID(id);
+                return View(model);
+        }
+        public ActionResult Edit(userdata user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.Updateuser(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("","Unable to save changes please try again");
+                
+            }
+            return View(user);
+        }
+        public ActionResult Delete(int id,bool? savechangeserror )
+        {
+            if (savechangeserror.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes. Try again";
+            }
+            userdata user = _repository.GetuserByID(id);
+            return View(user);
+
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+           
+            try
+            {
+                userdata model = _repository.GetuserByID(id);
+                _repository.Deleteuser(id);
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete", new System.Web.Routing.RouteValueDictionary {
+          { "id", id },
+          { "saveChangesError", true } });
+
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
